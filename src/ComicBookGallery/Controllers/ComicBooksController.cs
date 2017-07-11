d5ba25b5d9844cc94ee8953a0f9f17479dd4c057
stasmajevski <stas@tlu.ee>
2017-07-11 11:52:01 +0300

    updated detail actiom method

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from Stas");
+
+        }
+    }
+}
